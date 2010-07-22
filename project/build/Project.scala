0c952f24bf2531e3617493d6eded003aa1eb68f5
olim7t <omichallat+github@gmail.com>
2010-07-22 21:50:34 +0200

    Abstracted sxr config common to the managed and local options. Working with a local build of sxr for now, since there is a bug in the stable version (in my contributed code).

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,13 +2,11 @@ import sbt._
 import FileUtilities.{write, Newline}
 import com.github.olim7t.sbtscalariform._
 
-class SampleProject(info: ProjectInfo) extends DefaultProject(info) with SxrConfig with ScalariformConfig {
+class SampleProject(info: ProjectInfo) extends DefaultProject(info) with SxrLocalConfig with ScalariformConfig {
 	val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
 }
 
-trait SxrConfig extends BasicScalaProject with MavenStyleScalaPaths with AutoCompilerPlugins {
-  val sxr = compilerPlugin("org.scala-tools.sxr" %% "sxr" % "0.2.6")
-
+trait SxrBaseConfig extends BasicScalaProject with MavenStyleScalaPaths {
   // Links to external sxr sources (provided as file:// URLs)
   def externalLinks: Seq[String] = Seq()
 
@@ -23,7 +21,6 @@ trait SxrConfig extends BasicScalaProject with MavenStyleScalaPaths with AutoCom
     CompileOption("-P:sxr:link-file:" + linkFile.absolutePath) ::
     super.compileOptions.toList
 
-
   def linkFile = {
   	val linkFile = outputPath / "sxr.links"
   	if (!linkFile.exists) {
@@ -35,15 +32,22 @@ trait SxrConfig extends BasicScalaProject with MavenStyleScalaPaths with AutoCom
   }
 }
 
-// Alternative configuration to use a local build of sxr (not used here)
-trait SxrLocalConfig extends BasicScalaProject with MavenStyleScalaPaths {
-  val sxrPath = "/path/to/sxr_2.8.0-0.2.5.jar"
+// To handle sxr as a managed dependency :
+trait SxrAutoConfig extends SxrBaseConfig with AutoCompilerPlugins {
+  val sxr = compilerPlugin("org.scala-tools.sxr" %% "sxr" % "0.2.6")
+}
+
+// To use a local build of sxr :
+trait SxrLocalConfig extends SxrBaseConfig {
+  val sxrPath = "/tmp/sxr_2.8.0-0.2.6-SNAPSHOT.jar"
 
   override def compileOptions =
     CompileOption("-Xplugin:" + sxrPath) ::
-    CompileOption("-P:sxr:base-directory:" + mainScalaSourcePath.absolutePath) ::
-    CompileOption("-P:sxr:output-formats:vim") ::
     super.compileOptions.toList
+
+  override def testCompileOptions =
+    CompileOption("-Xplugin:" + sxrPath) ::
+    super.testCompileOptions.toList
 }
 
 trait ScalariformConfig extends BasicScalaProject with ScalariformPlugin {
