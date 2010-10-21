65ae0637f418885efae4a87e9d4487b826ae602b
olim7t <omichallat+github@gmail.com>
2010-10-21 23:25:55 +0200

    sbt-scalariform 1.0.2.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -3,7 +3,5 @@ import sbt._
 import java.net.URL
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val scalaToolsSnapshotRepo = "Scala-Tools Maven Repository" at "http://scala-tools.org/repo-snapshots"
-
-	val formatter = "com.github.olim7t" % "sbt-scalariform" % "1.0.1-SNAPSHOT"
+  val formatter = "com.github.olim7t" % "sbt-scalariform" % "1.0.2"
 }
